965c675fe7f824cbeb7cce98499df24351267e6f
Michael vieira <mike@MacBook-Pro-de-Michael.local>
2020-02-04 16:04:02 +0100

    added progress bar

---
+++ src/js/progress_bar.js
@@ -0,0 +1,46 @@
+var list = document.getElementById('progress'),
+    next = document.getElementById('next'),
+    clear = document.getElementById('clear'),
+    children = list.children,
+    completed = 1;
+
+// simulate activating a node
+next.addEventListener('click', function() {
+    
+    // count the number of completed nodes.
+    completed = (completed === 0) ? 1 : completed + 2;
+    if (completed > children.length) return;
+    
+    update();
+    
+}, false);
+
+clear.addEventListener('click', function() {  
+    completed -= 2;
+
+    update();
+}, false);
+
+function update() {
+  console.log(completed)
+    
+    for (var i = 0; i < children.length; i++) {
+
+      if (i < completed) {
+        children[i].children[0].classList.remove('grey');
+        children[i].children[0].classList.add('green');
+        
+        if (i % 2 === 0) {
+            children[i].children[0].classList.add('activated');            
+        }
+      } else {
+        children[i].children[0].classList.add('grey');
+        children[i].children[0].classList.remove('green');
+        
+        if (i % 2 === 0) {
+            children[i].children[0].classList.remove('activated');            
+        }
+      }
+      
+  }
+}
